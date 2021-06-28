1c01a6bf25c988d8ca1b2c7851029f5675f5368c
Kevin <pitzer.kevinj@gmail.com>
2021-06-27 22:05:44 -0400

    more scripts

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const addNewNote = (str) => {
   const parent = document.getElementById("note-grid");
   const newNote = document.createElement("div");
-  newNote.innerText = str;
+  const noteText = document.createElement("textarea");
+  noteText.setAttribute("readonly", "true");
+  noteText.innerHTML = str;
+  const editButton = document.createElement("button");
+  editButton.className = "edit-note";
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "delete-note";
+  editButton.innerText = "Edit";
+  deleteButton.innerText = "Delete";
+  newNote.appendChild(noteText);
+  newNote.appendChild(editButton);
+  newNote.appendChild(deleteButton);
+
   parent.appendChild(newNote);
+};
+
+const editNote = (node) => {
+  node.setAttribute("readonly", "false");
 };
 
 // Click handler for the add note button
@@ -14,3 +30,18 @@ addButton.addEventListener("click", (e) => {
   textarea.value = "";
   addNewNote(newNoteStr);
 });
+
+// Click handler to edit the clicked note
+/*
+THIS IS NOT RUNNING PROBS BECAUSE IT IS RUN BEFORE THE ELEMENTS EXIST
+*/
+const editbuttons = document.querySelectorAll(".edit-note");
+for (let i = 0; i < editbuttons.length; i++) {
+  editbuttons[i].addEventListener("click", (e) => {
+    const editArea = e.target.previousElementSibling;
+    console.log("edit: ", editArea);
+    editNote(editArea);
+  });
+}
+
+// Click handler to delete the clicked note
